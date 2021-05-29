6534c9963c2aa5a54e4d038b793cbf48cf1ccf96
Emiljan Dusha <emiljan.dusha@gmail.com>
2021-05-30 00:38:10 +0200

    added pacs function to fzf.sh

--- shell/.shell/functions/fzf.sh
+++ shell/.shell/functions/fzf.sh
@@ -1,5 +1,8 @@
 # view/vim file
-vf() { fzf | xargs -r -I % $EDITOR %; }
+vf() { fzf --preview='head -$LINES {}' | xargs -r -I % $EDITOR %; }
 
 # teleport
-tp() { cd "$(ls -R ~| grep "./" | sed "s|.$||" | fzf )"; }
+tp() { cd "$(ls -R ~| grep "./" | sed "s|.$||" | fzf )" && tree ; }
+
+# install packages
+pacs() { apt list | cut -d'/' -f1 | fzf -m | xargs sudo apt install -y ; }
